9b26375cbcfcc030fc42be9f21afb20c466ad162
Dan Hart <dan-hart@users.noreply.github.com>
2023-05-04 18:11:35 -0500

    Implement helper comments

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -5,30 +5,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("user")
+// You need to add the appropriate annotation to this class
 public class UserController {
-
-    @GetMapping("/add")
+    // You need to add the appropriate annotation to this method
     public String displayAddUserForm() {
-        return "user/add";
+        // Return the view name
+        return "";
     }
 
-    @PostMapping
+    // You need to add the appropriate annotation to this method
     public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        if (user.getPassword().equals(verify)) {
-           return "user/index";
-        }
-        else {
-            model.addAttribute("error", "Passwords do not match");
-            return "user/add";
-        }
-
+        // Add attributes to model and return view name
+        return "";
     }
-
-
 }
